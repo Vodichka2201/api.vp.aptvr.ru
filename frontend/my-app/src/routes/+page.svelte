1f16b7cd767de19f9ec41d44aa75b058794fa65e
Vodichka 2201 <prohozhevva@gmail.com>
2024-08-05 11:17:43 +0000

    tailwind-ok

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -8,12 +8,15 @@
 <h1>My Blog</h1>
   <div class="container mx-auto mt-4">
         {#each data.props.posts as post (post.id)}
-            <div
-                class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500"
-                on:click={() => goto('/blog/' + post.id)}
-            >
-                <h4 class="font-bold">{post.title}</h4>
-                <p class="mt-2 text-gray-800">{post.description}</p>
-            </div>
+        <div
+        class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500"
+        on:click={() => goto('/blog/' + post.id)}
+        on:keydown={(event) => event.key === 'Enter' && goto('/blog/' + post.id)}
+        role="button"
+        tabindex="0"
+      >
+        <h4 class="font-bold">{post.title}</h4>
+        <p class="mt-2 text-gray-800">{post.description}</p>
+      </div>
         {/each}
     </div>
