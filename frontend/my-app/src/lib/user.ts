bccba330800b5eb128f83049322c31b6794a2ddb
Vodichka2201 <prohozhevva@gmail.com>
2024-09-05 13:42:54 +0300

    go ubuntu

--- frontend/my-app/src/lib/user.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type { User } from './types';
-const user = writable<User | null>(null);
-export default user;
--- frontend/my-app/src/routes/login/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { Load } from '@sveltejs/kit';
-import type { PageData } from '../$types';
-
-export const load: Load = async ({ fetch }) => {
-  const res = await fetch('http://localhost:1337/posts');
-  const posts = await res.json();
-  return { props: { posts } } satisfies PageData;
-};
